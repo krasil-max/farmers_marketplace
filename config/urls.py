00038fbe5fb379c0106e442edb2a7ac1cc54e0ab
krasil-max <krasilsapphire@gmail.com>
2025-04-05 21:46:09 +0000

    update the config/urls.py to contain apps url

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+f# config/urls.py
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
+    path('api/users/', include('users.urls')),        # Users app
+    path('api/products/', include('products.urls')),  # Products app
+    path('api/locations/', include('locations.urls')),  # Locations app
+    path('api/reviews/', include('reviews.urls')),    # Reviews app
+    path('api/', include('api.urls')),                # Aggregated API views
 ]
+
