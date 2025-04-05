f74b00f62a3deec3e35dd5474c2f670c00ac9df4
krasil-max <krasilsapphire@gmail.com>
2025-04-05 19:16:48 +0000

    updated the models.py with product and category models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from users.models import FarmerProfile
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    farmer = models.ForeignKey(FarmerProfile, on_delete=models.CASCADE, related_name='products')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
+    inventory_count = models.PositiveIntegerField(default=0)
+    average_rating = models.FloatField(default=0.0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
